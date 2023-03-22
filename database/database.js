87aa70916d1209bd0a9e9f07d02cda8ab769e4b1
Julia Luciano Do Nascimento <780489@senacminas.edu.br>
2023-03-22 10:34:43 -0300

    initial commit

---
+++ database/database.js
@@ -0,0 +1,16 @@
+//arquivo que faz conexoa com o banco de dados 
+require("dotenv").config()
+
+//ORN que auxilia na conexão com o banco de dados utilizando o javascript
+const Sequelize = require('sequelize') // importando o sequelize
+
+//constante que cria conexão
+const connection = new Sequelize(process.env.DB_NAME, process.env.USER, process.env.PASSWORD, {
+    host: process.env.HOST,
+    port: process.env.PORT,
+    dialect: 'mysql'
+})
+
+// para exportar o modo de conexão com o bnco de dados para poder utilizar no arquivo 
+// index.js
+module.exports = connection;
